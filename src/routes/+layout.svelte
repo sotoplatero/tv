bc3b946f392340d33f01e7864186dd221336b685
unknown <soto.platero@gmail.com>
2025-10-23 09:18:18 -0400

    Deploy

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,7 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"	>
 </svelte:head>
 
 {@render children?.()}
